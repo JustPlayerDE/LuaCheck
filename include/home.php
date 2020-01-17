9ec8e66b6bd362e44e71944decfa7d575e893699
Justin Klüver <justin.k@justplayer.de>
2020-01-17 13:49:54 +0100

    Update upload form

--- include/home.php
+++ include/home.php
@@ -3,8 +3,13 @@
 <p>This tool <b>cant</b> detect real backdoors or exploits, it only tells you where you <b>may</b> find them</p>
 <br />
 <form action="?p=check" method="post" enctype="multipart/form-data">
-    <input class="control-label col-sm-5" type="file" name="datei">
-    <input type="submit" name="submit_file">
+
+    <fieldset class="form-group">
+        <label for="file">Choose your file. <small>Max file size: <?= $Config['FileSize'] / 1024;?> KBytes</small></label>
+        <input class="form-control col-sm-5" id="file" type="file" name="datei">
+    </fieldset>
+    <input type="submit" name="submit_file" class="btn btn-block" value="Upload & Check">
+
 </form>
 
 <script type="text/javascript">
