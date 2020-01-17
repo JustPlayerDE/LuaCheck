2f8b8cc788a91c4be3260ae5f1f09a49dc8ea7b4
Justin Klüver <justin.k@justplayer.de>
2020-01-17 11:07:11 +0100

    Fix some html issues in home.php

--- include/home.php
+++ include/home.php
@@ -1,16 +1,12 @@
 <h1>LuaCheck:</h1>
-<p>Welcome to the LuaChecker! this site will check your code for maybe harmful code<p>
-<p>I build this site in my freetime for checking my scripts before i add him to the Server<p>
+<p>Welcome to the LuaChecker! this site will check your code for maybe harmful code</p>
+<p>I build this site in my freetime for checking my scripts before i add him to the Server</p>
 <p>but first i want to say: please note that this site do not can warn you about Backdoors or something it says only what inside</p>
 <p>Im open for reports how i can make it better or what are buggy ^^</p>
-<br/>
-
-
-
-
+<br />
 <form action="?p=check" method="post" enctype="multipart/form-data">
-<h4>Check Your Code (Max 2Mb, ZIP):</h4>
-<input class="control-label col-sm-5" type="file" name="datei"> <input type="submit" name ="lelelrs">
+    <h4>Check Your Code (Max 2Mb, ZIP):</h4>
+    <input class="control-label col-sm-5" type="file" name="datei"> <input type="submit" name="lelelrs">
 </form>
 <br>
 <p>Please note that we do not save any content that you upload! the only space in time where your file are located is in the default PHP temporary storage in a by the Doctor secured Box named "Tardis" ;)</p>
@@ -18,7 +14,8 @@
 <p>However, please report any errors! and <b>I am not liable for any damage by the scripts that you uploaded</b></p>
 
 
-<p>Version:</p><pre>1.0.0 ALPHA</pre>
+<p>Version:</p>
+<pre>1.0.0 ALPHA</pre>
 
 
 <a href="#" onclick="toggle_visibility('Advanced');">Show some Debug information</a>
@@ -29,17 +26,14 @@ print_r($_SESSION);
 echo '<br>$_POST:';
 print_r($_POST);
 ?>
-
 </pre>
 
 <script type="text/javascript">
-<!--
     function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
+        var e = document.getElementById(id);
+        if (e.style.display == 'block')
+            e.style.display = 'none';
+        else
+            e.style.display = 'block';
     }
-//-->
 </script>
